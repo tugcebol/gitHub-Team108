41a8ad352f3a0591c390377542c65c9c04c02017
tugcebol <boltugce8@gmail.com>
2022-12-28 10:45:01 +0300

    v2

--- src/C01.java
+++ src/C01.java
@@ -3,5 +3,11 @@ public class C01 {
     public static void main(String[] args) {
 
         System.out.println("Hello gitHub");
+
+        System.out.println("CMD çok kullanışlı");
+
+        System.out.println("Bugünün sonunu görebilecek miyiz?");
+
+
     }
 }
